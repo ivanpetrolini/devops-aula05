a52a3bc4cb6cf811343c5504e33c446e74faa233
ivanpetrolini <36649167+ivanpetrolini@users.noreply.github.com>
2018-03-12 21:59:31 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -12,8 +12,8 @@ else:
         for elemento in linha:
           if elemento != '.':
            erroInicializar = True
-    if erroInicializar:
-     sys.exit(1)
-    else:
-     sys.exit(0)
+if erroInicializar:
+  sys.exit(1)
+else:
+  sys.exit(0)
 
